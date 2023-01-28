82ca4928d5b873f4a7eaac664147512978e2281d
nursalimdev <nursalimdev123@gmail.com>
2023-01-28 19:17:35 +0700

    number

---
+++ src/mypackage/classes/NumberApp.java
@@ -0,0 +1,21 @@
+package mypackage.classes;
+
+public class NumberApp {
+    public static void main(String[] args) {
+        Integer intValue = 10;
+        Long longValue = intValue.longValue();
+        Double doubleValue = intValue.doubleValue();
+        Short shortValue = doubleValue.shortValue();
+
+        String contoh = "10000";
+
+        Integer contohInteger = Integer.valueOf(contoh);
+        System.out.println(contohInteger);
+
+        int contohInt = Integer.parseInt(contoh);
+        System.out.println(contohInt);
+
+        
+
+    }
+}
