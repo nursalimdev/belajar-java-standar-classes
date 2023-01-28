99812fa7dbc360d04b0782fb98be49847096ac6e
nursalimdev <nursalimdev123@gmail.com>
2023-01-28 19:23:44 +0700

    math

--- src/mypackage/classes/MathApp.java
+++ src/mypackage/classes/MathApp.java
@@ -2,6 +2,9 @@ package mypackage.classes;
 
 public class MathApp {
     public static void main(String[] args) {
-        
+        System.out.println(Math.min(1000, 1500));
+        System.out.println(Math.max(1000, 1500));
+
+        System.out.println(Math.PI);
     }
 }
