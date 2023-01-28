7e90eda3bfab4fa6dda9442a45e753709d54ce11
nursalimdev <nursalimdev123@gmail.com>
2023-01-28 16:26:18 +0700

    string joiner

---
+++ src/mypackage/classes/StringJoinerApp.java
@@ -0,0 +1,14 @@
+package mypackage.classes;
+
+import java.util.StringJoiner;
+
+public class StringJoinerApp {
+    public static void main(String[] args) {
+        String[] names = {"Nursalim", "Al", "Farizi"};
+        StringJoiner joiner = new StringJoiner(", ", "[", "]");
+        for(var name: names){
+            joiner.add(name);
+        }
+        System.out.println(joiner.toString());
+    }
+}
